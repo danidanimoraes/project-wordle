b9cbfcc5bc646512067983dfb5e81177e3e88a20
Daniele de Moraes <daniele.barbosa@ifood.com.br>
2023-09-25 04:27:46 -0300

    Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const initialGuess = "";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState(initialGuess);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess(initialGuess);
+  };
+
+  const handleChange = (e) => {
+    if (guess.length >= 5) {
+      alert("Maximum of 5 letters!");
+    } else {
+      setGuess(e.target.value.toUpperCase());
+    }
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
