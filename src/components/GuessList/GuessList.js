0aa8c550afdf27fc6de884b6916faac4311c14dc
Daniele de Moraes <daniele.barbosa@ifood.com.br>
2023-09-25 05:09:32 -0300

    Exercise 3: Show results in grid

--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+
+const getEmptyArray = (size) => {
+  return Array(size).fill();
+};
 
 function GuessList({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => {
+      {getEmptyArray(NUM_OF_GUESSES_ALLOWED).map((_row, rowIndex) => {
+        const rowGuess = guesses[rowIndex];
         return (
-          <p className="guess" key={guess.id}>
-            {guess.value}
+          <p className="guess" key={`guessRow-${rowIndex}`}>
+            {getEmptyArray(5).map((_col, colIndex) => {
+              return (
+                <span
+                  className="cell"
+                  key={`guessCell-${rowIndex}.${colIndex}`}
+                >
+                  {rowGuess?.value[colIndex]}
+                </span>
+              );
+            })}
           </p>
         );
       })}
